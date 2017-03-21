34a706356e92a2dfd6cc19e969d0ca4857f1c4fc
Anton_000 <Anton_000@192.168.0.5>
2017-03-21 23:40:35 +0100

    Added displayable window, gameloop, input support.

---
+++ Main/src/main/java/hills/Anton/engine/display/AspectRatios.java
@@ -0,0 +1,64 @@
+package hills.Anton.engine.display;
+
+public final class AspectRatios {
+	/**
+	 * Square
+	 */
+	public static final double ONE_TO_ONE = 1.0;
+	
+	/**
+	 * Fox Movie tone aspect ratio
+	 */
+	public static final double SIX_TO_FIVE = 5.0 / 6.0;
+	
+	/**
+	 * Early television & large-format computer monitors
+	 */
+	public static final double FIVE_TO_FOUR = 4.0 / 5.0;
+	
+	/**
+	 * Traditional television & computer monitor standard
+	 */
+	public static final double FOUR_TO_THREE = 3.0 / 4.0;
+	
+	/**
+	 * Academy standard film aspect ratio
+	 */
+	public static final double ELEVEN_TO_EIGHT = 8.0 / 11.0;
+	
+	/**
+	 * Classic 35mm still photographic film
+	 */
+	public static final double THREE_TO_TWO = 2.0 / 3.0;
+	
+	/**
+	 * A common computer screen ratio
+	 */
+	public static final double SIXTEEN_TO_TEN = 10.0 / 16.0;
+	
+	/**
+	 * The golden ratio
+	 */
+	public static final double SIXTEEN_POINT_ONE_EIGHT_TO_TEN = 10.0 / 16.18;
+	
+	/**
+	 * A common European wide screen standard.; Native Super 16mm film.
+	 */
+	public static final double FIVE_TO_THREE = 3.0 / 5.0;
+	
+	/**
+	 * HD video standard.; U.S. digital broadcast TV standard.
+	 */
+	public static final double SIXTEEN_TO_NINE = 9.0 / 16.0;
+	
+	/**
+	 * A current wide screen cinema standard
+	 */
+	public static final double TWO_POINT_THREE_FIVE_TO_ONE = 1.0 / 2.35;
+	
+	/**
+	 * The silver ratio
+	 */
+	public static final double TWO_POINT_FOUR_ONE_FOUR_TO_ONE = 1.0 / 2.414;
+
+}
--- Main/src/main/java/hills/Main.java
+++ Main/src/main/java/hills/Main.java
@@ -1,10 +1,15 @@
 package hills;
 
+import hills.Anton.Init;
+
 /**
  * Created by gustav on 2017-03-21.
  */
 public class Main {
     public static void main(String [] args){
         System.out.println("launching main application");
+        
+        Init i = new Init();
+        i.init();
     }
 }
