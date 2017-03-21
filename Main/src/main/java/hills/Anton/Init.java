f8edf651f35f15a9d60df06eaadcf18571273e5a
Anton_000 <Anton_000@192.168.0.5>
2017-03-21 20:42:39 +0100

    test push

--- Main/src/main/java/hills/Anton/Init.java
+++ Main/src/main/java/hills/Anton/Init.java
@@ -1,7 +1,16 @@
 package hills.Anton;
 
+import lombok.Getter;
+
 /**
  * Created by gustav on 2017-03-21.
  */
 public class Init {
+	
+	@Getter int age = 5;
+	
+	public Init(){
+		System.out.println(getAge());
+	}
+	
 }
