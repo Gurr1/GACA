8c8f04eff19bfbadca0a9ebd58a17ed73bce2d69
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2017-03-28 12:12:16 +0200

    added the testfile to directories

--- Main/src/test/java/terrainTest.java
+++ Main/src/test/java/terrainTest.java
@@ -17,29 +17,29 @@ public class terrainTest {
     @Before
     public void testTerrain(){
         Random random = new Random();
-            t = new Terrain(random.nextLong());
-            assertNotNull(t);
+        t = new Terrain(random.nextLong());
+        assertNotNull(t);
     }
     @Test
     public void testCreateHeightMap(){
-            int[][] terrain = t.createHeightMap();
-            int smallest = 100;
-            int largest = 100;
-            for(int x = 0; x<terrain.length; x++){
-                for (int y = 0; y < terrain[0].length; y++){
-                    assertTrue(terrain[x][y]<=255);
-                    assertTrue(terrain[x][y]>=0);
-                    if(smallest>terrain[x][y]){
-                        smallest = terrain[x][y];
-                    }
-                    if(largest<terrain[x][y]){
-                        largest = terrain[x][y];
-                    }
+        int[][] terrain = t.createHeightMap();
+        int smallest = 100;
+        int largest = 100;
+        for(int x = 0; x<terrain.length; x++){
+            for (int y = 0; y < terrain[0].length; y++){
+                assertTrue(terrain[x][y]<=255);
+                assertTrue(terrain[x][y]>=0);
+                if(smallest>terrain[x][y]){
+                    smallest = terrain[x][y];
+                }
+                if(largest<terrain[x][y]){
+                    largest = terrain[x][y];
                 }
             }
+        }
         System.out.println(largest);
         System.out.println(smallest);
         assertTrue(largest>250);
         assertTrue(smallest<110);
     }
-    }
+}
