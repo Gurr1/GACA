4d58af68732f3d838c0cc053176fbc3f77e8d7e9
annlova <Anton.Annlov@gmail.com>
2017-03-30 12:26:06 +0200

    quick version fix

--- Main/src/main/java/hills/engine/system/terrain/TerrainSystem.java
+++ Main/src/main/java/hills/engine/system/terrain/TerrainSystem.java
@@ -18,7 +18,7 @@ public class TerrainSystem extends EngineSystem {
 	private static TerrainSystem instance = null;
 	
 	public static final float MORPH_FACTOR = 0.9f;
-	public static final float[] RANGES = {16.0f, 32.0f, 64.0f, 128.0f, 256.0f, 512.0f, 1024.0f, 2048.0f, 4096.0f, 8192.0f};
+	public static final float[] RANGES = {16.0f, /*32.0f,*/ 64.0f, /*128.0f,*/ 256.0f, /*512.0f,*/ 1024.0f, /*2048.0f,*/ 4096.0f, /*8192.0f,*/ 8000.0f, 8000.0f, 8000.0f};
 	public static final float[] SCALES = {1.0f, 2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0f, 512.0f};
 	public static final int GRID_WIDTH = 16;
 	public static final int GRID_DEPTH = 16;
